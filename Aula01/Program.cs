45d4195bd34787bad74fa6aaab10a7c066739b94
Ueslei Antunes <antunesueslei5@gmail.com>
2024-10-08 21:35:34 -0300

    add aula 01

---
+++ Aula01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula01.Console.Estruturado
+{ 
+    internal class Program
+    {
+        private static void Main(string[] args)
+            {
+                System.Console.WriteLine("Hello, World!");
+            }
+        } 
+    }

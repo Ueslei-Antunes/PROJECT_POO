baed9ae8c7b56ef007851e4e8a4e9dfd23c33f0d
RicardoHSimoni <ricardosimoni87@gmail.com>
2024-10-10 11:40:44 -0300

    1.0

--- AppGestão/obj/Debug/net8.0/AppGestão.AssemblyInfo.cs
+++ AppGestão/obj/Debug/net8.0/AppGestão.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGestão")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec31ef583d8481368325b22c399af7d70b7ea499")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGestão")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGestão")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

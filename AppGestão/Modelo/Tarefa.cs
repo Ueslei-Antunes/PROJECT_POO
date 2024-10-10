a97e70517486dff35c826272164203f882e638e5
Ueslei Antunes <antunesueslei5@gmail.com>
2024-10-10 12:18:50 -0300

    Update Function Status tarefas.cs

---
+++ AppGestão/Modelo/Tarefa.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace AppGestão.Modelo;
+
+public class Tarefa
+{
+    public string Prazo { get; set; }
+
+    public string Descricao { get; set; }
+
+    public string Prioridade { get; set; }
+
+    public string Categoria { get; set; }
+
+    public string Status { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Prazo},{Descricao}, {Prioridade},{Categoria}, {Status}]";
+    }
+}

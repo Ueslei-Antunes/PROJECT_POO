aa100c8c615e870a9c327a4ee1d9c208135ab6a2
RicardoHSimoni <ricardosimoni87@gmail.com>
2024-11-02 11:24:09 -0300

    Atualização Cadastro categoria
    Adicionado um botão com um prompt para cadastrar uma nova categoria e modificado o Picker de exibição das categorias no Register

--- UI/Register.xaml.cs
+++ UI/Register.xaml.cs
@@ -2,13 +2,36 @@ namespace UI;
 
 public partial class Register : ContentPage
 {
-public Register()
-{
-	InitializeComponent();
-	}
+  private List<string> opcoes = new();
+  public Register()
+  {
+    InitializeComponent();
 
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-		await Navigation.PopAsync();
+    opcoes.Add("Estudo");
+    opcoes.Add("Rotinas");
+    opcoes.Add("Prioritaria");
+   
+  
+    CategoryPicker.ItemsSource = opcoes;
+  }
+  private async void Button_Clicked(object sender, EventArgs e){
+	  await Navigation.PopAsync();
+  }
+
+   private async void OnNewCategoryClicked(object sender, EventArgs e){
+      
+    string category = await DisplayPromptAsync("Nova Categoria", "Digite o nome da nova categoria:");
+
+     if (!string.IsNullOrWhiteSpace(category))
+        {
+            opcoes.Add(category);
+
+            CategoryPicker.ItemsSource = null;
+            CategoryPicker.ItemsSource = opcoes;
+        }
+        else
+        {
+            await DisplayAlert("Erro", "Por favor, digite uma categoria válida.", "OK");
+        }
     }
 }

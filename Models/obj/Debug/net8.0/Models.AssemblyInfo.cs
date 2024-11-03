9a05eeba86ca68f316e4e0bb7269cb90da548395
Ueslei Antunes <antunesueslei5@gmail.com>
2024-11-03 19:21:44 -0300

    Update Interface

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa100c8c615e870a9c327a4ee1d9c208135ab6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d36d3b079280e9de82e8007498d9541eeca201")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UI/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aa100c8c615e870a9c327a4ee1d9c208135ab6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b0d36d3b079280e9de82e8007498d9541eeca201")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
